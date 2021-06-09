3fb769774d5d8c9512fc343e93e63daa9b0d2781
Jairo Laupa <jlaupa@blended.com.ar>
2021-06-09 12:01:27 +0200

    feat: create dog park table

---
+++ database/migrations/2021_06_09_115245_create_dog_park_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDogParkTable extends Migration
+{
+    private const TABLE = 'dog_park';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE, function (Blueprint $table) {
+            $table->unsignedBigInteger('dog_id');
+
+            $table->unsignedBigInteger('park_id');
+
+            $table->foreign('dog_id')
+                ->references('id')
+                ->on('dogs')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+}
