650bd911050b4b735f705996aa974726d82300a8
Jairo Laupa <jlaupa@blended.com.ar>
2021-06-10 08:13:47 +0000

    feat: add, list OwnersWithDogs and force leave owner
    BEE-1

--- app/Models/Dog.php
+++ app/Models/Dog.php
@@ -3,18 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @property mixed name
  * @mixin IdeHelperDog
+ * @method static filterOwner($dogsId, $ownerId)
  */
 class Dog extends Model
 {
     use HasFactory;
 
-    public function owner()
+    protected $fillable = [
+        'name',
+        'owner_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function owner(): BelongsTo
     {
         return $this->belongsTo(Owner::class);
+    }
+
+    /**
+     * @param Builder $query
+     * @param array $dogsId
+     * @param int $ownerId
+     * @return Builder|\Illuminate\Database\Query\Builder
+     */
+    public function scopeFilterOwner(Builder $query, array $dogsId, int $ownerId)
+    {
+        return $query->whereIn('id', array_unique($dogsId))
+            ->where('owner_id', $ownerId);
     }
 }
