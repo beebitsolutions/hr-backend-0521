12f039b4007af9ccd5fe523a4c01a8d1e7781bdf
Juan <juanclb93@gmail.com>
2021-06-11 03:04:19 +0200

    Optimizado DogFactory. El tiempo de db:seed se ha dividido por 20.

--- database/factories/DogFactory.php
+++ database/factories/DogFactory.php
@@ -5,6 +5,8 @@ namespace Database\Factories;
 use App\Models\Dog;
 use App\Models\Owner;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class DogFactory extends Factory
 {
@@ -27,5 +29,27 @@ class DogFactory extends Factory
             'name' => $this->faker->name(),
             'owner_id' => $ownerId
         ];
+    }
+
+    
+    public function create($attributes = [], ?Model $parent = null){
+        $count = $this->state($attributes)->count;
+
+        for ($i=0; $i < $count; $i++) {
+            $ownerId = Owner::all()->random(1)[0]->id;
+            $dogData[] = [
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'name' => $this->faker->name,
+                'owner_id' => $ownerId
+            ];
+        }
+
+
+        $chunks = array_chunk($dogData, 50);
+
+        foreach ($chunks as $chunk) {
+            Dog::insert($chunk);
+        }
     }
 }
